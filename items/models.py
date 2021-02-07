e0fcb023f073f35ef739e1d08768638ef9474290
Vincent Catros <vincent.catros@laposte.net>
2021-02-07 21:06:08 +0100

    models added

---
+++ items/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from members.models import *
+
+# Item types.
+class ItemType(models.Model):
+    vendor = models.CharField(max_length=20, null=True)
+    type = models.CharField(max_length=20)
+    description = models.CharField(max_length=30)
+
+# Items
+class Item(models.Model):
+  item = models.CharField(max_length=20)
+  item_type = models.ForeignKey(ItemType, on_delete=models.PROTECT)
+  serial = models.CharField(max_length=20, null=True)
+  size = models.CharField(max_length=5, null=True)
+  member = models.ForeignKey(Member, null=True, on_delete=models.SET_NULL)
+  commissioning_date = models.DateField(null=True)
+  comment = models.CharField(max_length=50, null=True)
