de38169032b9a2efd277996a8a51f1e9f75b0590
Vincent Catros <vincent.catros@laposte.net>
2021-02-08 10:44:52 +0100

    Comments added

--- items/urls.py
+++ items/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Item types
     path('types', views.types_listing),
     path('types/<int:type_id>', views.type_detail),
     path('add-type', views.type_add),
+
+    # Items
     path('items', views.items_listing),
     path('items/<int:item_id>', views.item_detail),
     #path('<int:member_id>', views.detail),
