f318ef410d89ee4ea1e4eeae2b09cbeb9dd2429a
Vincent Catros <vincent.catros@laposte.net>
2021-02-13 20:38:44 +0100

    Comment added

--- members/urls.py
+++ members/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Members
     path('', views.listing),
     path('<int:member_id>', views.detail),
     path('search/', views.search),
