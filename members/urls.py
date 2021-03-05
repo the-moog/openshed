36d732a418d91b8027485cf8c0f7edc4f00810e4
Vincent Catros <vincent.catros@laposte.net>
2021-03-05 19:30:47 +0100

    Members addition and edition added

--- members/urls.py
+++ members/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # Members
     path('members', views.members_listing),
     path('members/<int:member_id>', views.detail),
+    path('members/add', views.member_add),
+    path('members/<int:id>/edit', views.member_edit),
+    path('members/<int:id>/delete', views.member_delete),
     path('search/', views.search),
 ]
--- members/views.py
+++ members/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Q
 
 from .models import Member
+from .forms import MemberForm
 
 # Create your views here.
 
@@ -24,6 +25,53 @@ def detail(request, member_id):
     }
 
     return render(request, 'members/member.html', context)
+
+def member_add(request):
+    if request.method == 'POST':
+        form = MemberForm(request.POST)
+
+        if form.is_valid():
+            member = Member()
+
+            member.first_name = form.cleaned_data['first_name']
+            member.last_name = form.cleaned_data['last_name']
+
+            member.save()
+
+            return redirect(f'/members/members/{member.id}')
+
+    else:
+        form = MemberForm()
+
+    return render(request, 'members/member-edit.html', {'form': form})
+
+def member_edit(request, id):
+    member = Member.objects.get(pk=id)
+
+    if request.method == 'POST':
+        form = MemberForm(request.POST)
+
+        if form.is_valid():
+            member.first_name = form.cleaned_data['first_name']
+            member.last_name = form.cleaned_data['last_name']
+
+            member.save()
+
+            return redirect(f'/members/members/{member.id}')
+
+    else:
+        form = MemberForm(initial={'first_name': member.first_name,
+                                   'last_name': member.last_name})
+
+    return render(request, 'members/member-edit.html', {'form': form})
+
+def member_delete(request, id):
+    if request.method == 'POST':
+        Member.objects.get(pk=id).delete()
+
+        return redirect('/members/members')
+
+    return render(request, 'members/member-delete.html')
 
 def search(request):
     query = request.GET.get('query')
