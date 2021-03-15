ce32c2426731adeb488540244d71d41156f62feb
Vincent Catros <vincent.catros@laposte.net>
2021-03-15 19:23:01 +0100

    file added

---
+++ members/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import Member
+
+class MemberForm(forms.Form):
+    last_name = forms.CharField(max_length=20)
+    first_name = forms.CharField(max_length=20)
