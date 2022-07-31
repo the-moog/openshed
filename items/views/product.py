80565ca3f29bba853851632a2d56522f07638cac
the-moog <3725802+the-moog@users.noreply.github.com>
2022-07-31 13:22:13 +0100

    Refactor out the use of item_type as 'type' (used everywhere) is a reserved word in Python.and product better describes it's use.

---
+++ items/views/product.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from items.models import Product, Item
+from items.forms import ProductForm
+
+
+@login_required
+def products_listing(request):
+    products = Product.objects.all()
+
+    if request.GET.get('category') is not None:
+        products = products.filter(category=request.GET.get('category'))
+
+    if request.GET.get('vendor') is not None:
+        products = products.filter(vendor=request.GET.get('vendor'))
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'items/products.html', context)
+
+
+@login_required
+def product_detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    item_count = Item.objects.filter(product=product).count()
+
+    context = {
+        'product': product,
+        'item_count': item_count
+    }
+
+    return render(request, 'items/product.html', context)
+
+
+@login_required
+def product_add(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            product = Product()
+
+            product.category = form.cleaned_data['category']
+            product.vendor = form.cleaned_data['vendor']
+            product.name = form.cleaned_data['name']
+            product.description = form.cleaned_data['description']
+
+            product.save()
+
+            return redirect(f'/items/products/{product.id}')
+
+    else:
+        form = ProductForm()
+
+    return render(request, 'items/product-edit.html', {'form': form})
+
+
+@login_required
+def product_edit(request, id):
+    product = Product.objects.get(pk=id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            product.category = form.cleaned_data['category']
+            product.vendor = form.cleaned_data['vendor']
+            product.name = form.cleaned_data['name']
+            product.description = form.cleaned_data['description']
+
+            product.save()
+
+            return redirect(f'/items/products/{product.id}')
+
+    else:
+        form = ProductForm(initial={'category': product.category,
+                                 'vendor': product.vendor,
+                                 'name': product.name,
+                                 'description': product.description})
+
+    return render(request, 'items/product-edit.html', {'form': form, 'obj': product})
+
+
+@login_required
+def product_delete(request, id):
+    if request.method == 'POST':
+        Product.objects.get(pk=id).delete()
+
+        return redirect('/items/products')
+
+    return render(request, 'items/product-delete.html')
